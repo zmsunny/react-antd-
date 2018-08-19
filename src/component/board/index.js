a6b31c1cb4aed00ea855044bc3391e2208248256
zmsunny <zhumisunny@163.com>
2018-08-19 21:15:22 +0800

    完成增删查改

--- src/component/board/index.js
+++ src/component/board/index.js
@@ -1,13 +1,15 @@
 import React, {Component} from 'react'
-import {Model, Table, Button, Divider, Icon, Select, Modal } from 'antd';
+import {Model, Table, Button, Divider, Icon, Select, Modal,Input} from 'antd';
 const Option = Select.Option;
-
+const {TextArea} = Input;
 
 class Board extends Component {
     constructor(props){
         super(props)
         this.searchChange = this.searchChange.bind(this)
         this.toggleModal = this.toggleModal.bind(this)
+        this.createBoard = this.createBoard.bind(this)
+        this.submitCreateBoard = this.submitCreateBoard.bind(this)
     }
     state = {
         selectedRowKeys: [], // Check here to configure the default column
@@ -31,7 +33,8 @@ class Board extends Component {
                     <span>
                         <a href="javascript:;">修改</a>
                         <Divider type="vertical" />
-                        <a href="javascript:;">删除</a>
+                        <a onClick = { this.deleteBoard.bind(this,record.id) }
+                        href = "javascript:;" > 删除 </a>
                         <Divider type="vertical" />
                         <a onClick={this.checkDetail.bind(this,record.id)} href="javascript:;" className="ant-dropdown-link">
                         查看
@@ -76,6 +79,16 @@ class Board extends Component {
         }
         this.toggleModal()
     }
+    deleteBoard(id) {
+         //这里需要和后台配合先连接数据库后然后请求后台api删除数据之后然后页面上再去删除
+         this.setState({
+             data: this.state.data.filter(item => item.id !== id)
+         })
+    }
+    createBoard(){
+        this.setState({check_data:{}})
+        this.toggleModal()
+    }
     start = () => {
         this.setState({ loading: true });
         // ajax request after empty completing
@@ -87,15 +100,22 @@ class Board extends Component {
         }, 1000);
     }
 
-  onSelectChange = (selectedRowKeys) => {
-    console.log('selectedRowKeys changed: ', selectedRowKeys);
-    this.setState({ selectedRowKeys });
-  }
-  searchChange(value){
-      this.getData(value)
-  }
+    onSelectChange = (selectedRowKeys) => {
+        console.log('selectedRowKeys changed: ', selectedRowKeys);
+        this.setState({ selectedRowKeys });
+    }
+    searchChange(value){
+        this.getData(value)
+    }
+    submitCreateBoard(title, content) {
+        let id = this.state.data.length + 1
+        let board = { title, content, type: '生活园地', type_id: 1, id, key: id }
+        this.state.data.push(board)
+        this.setState({ data: this.state.data })
+        this.toggleModal()
+    }
   render() {
-      const { loading, selectedRowKeys, data, types, check_data} = this.state;
+    const { loading, selectedRowKeys, data, types, check_data} = this.state;
     const rowSelection = {
       selectedRowKeys,
       onChange: this.onSelectChange,
@@ -121,21 +141,66 @@ class Board extends Component {
           >
             Reload
           </Button>
+          <Button
+            type="primary"
+            onClick={this.createBoard}
+          >
+            create
+          </Button>
           <span style={{ marginLeft: 8 }}>
             {hasSelected ? `Selected ${selectedRowKeys.length} items` : ''}
           </span>
         </div>
         <Table rowSelection={rowSelection} columns={this.state.columns} dataSource={data} />
-            <Modal
-                title={check_data.title}
+            <OAModel check_data={check_data}
                 visible={this.state.modal_visible}
-                onOk={this.toggleModal}
-                onCancel={this.toggleModal}
-            >
-                <p>{check_data.content}</p>
-            </Modal>
+                onOk={ check_data.title ? this.toggleModal : this.submitCreateBoard }
+                onCancel={this.toggleModal}>
+		</OAModel>  
       </div>
     );
   }
+}
+const CreateFrom = ({get_title,get_content}) =>{
+	return (
+		<div>
+		  <Input  onChange={get_title}  placeholder="please enter your title" />
+		  <TextArea  onChange={get_content} placeholder="please enter your content" autosize={{ minRows: 3 }} />
+		</div>
+	)
+}
+class OAModel extends Component{
+    state = {
+        board:{title:'',content:''}
+    }
+    constructor(props){
+        super(props)
+        this.get_title = this.get_title.bind(this)
+        this.get_content = this.get_content.bind(this)
+    }
+    get_title(e){
+        this.state.board.title = e.target.value
+        this.setState({board:this.state.board})
+    }
+    get_content(e){
+        this.state.board.content = e.target.value
+        this.setState({board:this.state.board})
+    }
+    render(){
+        let title = this.props.check_data.title || '新增'
+        let content = this.props.check_data.content || <CreateFrom get_title={this.get_title} get_content={this.get_content}></CreateFrom>
+        return (
+            <Modal
+                title={title}
+                visible={this.props.visible}
+                onOk = {
+                    this.props.onOk.bind(null,this.state.board.title,this.state.board.content)
+                }
+                onCancel={this.props.onCancel}
+            >
+                <div>{content}</div>
+            </Modal>
+        )
+    }
 }
 export default Board
--- src/component/leavework/index.js
+++ src/component/leavework/index.js
@@ -1,12 +1,47 @@
 import React, {Component} from 'react'
- 
+import './index.scss'
+import echarts from 'echarts'
+
+
 class Leavework extends Component{
+    constructor(props){
+        super(props)
+    }
+    state={
+        option:{
+            title: {
+                text: '工资收入'
+            },
+            tooltip: {},
+            grid:{
+                left:50,
+                x:45
+            },
+            xAxis: {
+                axisTick: { length:15 },
+                data: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月']
+            },
+            yAxis: {},
+            series: [{
+                name: '工资',
+                type: 'bar',
+                barGap:5,
+                barWidth:30,
+                data: [5100, 2065, 3634, 6500, 10235, 5100, 2065, 3634, 6500, 10235, 6500, 10235]
+            }]  
+        }
+    }
     render(){
         return (
-            <div className="app-leavework">
-                this is a Leavework
+            <div className="leavework">
+                <div ref={el => this.el = el} className="content"></div>
             </div>
         )
+    }
+    componentDidMount() {
+       
+        this.chart = echarts.init(this.el)
+        this.chart.setOption(this.state.option,true)
     }
 }
 export default Leavework
